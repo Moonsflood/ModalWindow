82deca55a2efaec9117c75a310ad07216e088a4d
Aysel <ayselk94@gmail.com>
2021-09-15 16:50:58 -0400

    Adding in functions for closing the modal - closing via click on the 'x' button or by clicking outside of the modal window

--- script.js
+++ script.js
@@ -1,8 +1,29 @@
 'use strict';
 
-//Element with class modal
+//Elements with class modal
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btn = document.querySelector('.close-modal');
 const btnsOpenModal = document.querySelectorAll('.show-modal');
-console.log(btnsOpenModal);
+const btnCloseModal = document.querySelector('.close-modal');
+
+//Function for closing the modal 
+const openModal = function () {
+  console.log('Button clicked');
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+}
+
+//Function for closing the modal 
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+}
+
+//Going through the modals 
+for (let i = 0; i < btnsOpenModal.length; i++)
+  btnsOpenModal[i].addEventListener('click', openModal);
+
+//executing closing the modal 
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
